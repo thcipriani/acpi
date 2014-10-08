8d9a7cec5682eb425e2888084a6b2be070fb5782
Tyler Cipriani <thcipriani@gmail.com>
2014-10-07 21:51:11 -0600

    Working now

--- handler.sh
+++ handler.sh
@@ -3,6 +3,8 @@
 user="$(w -h -s | awk '/:[0-9]/ {print $1}' | head -1)"
 userhome=$(getent passwd $user | cut -d: -f6)
 
+logger "ACPI Event: $@"
+
 ACPI_Event() {
   case $1 in
     power)
@@ -24,31 +26,29 @@ ACPI_Event() {
     tablet)
       su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/display tabletmode"
       ;;
-    stylus
-      case $2 in
-        eject) ;;
-        replace) ;;
-      esac
-      ;;
-    ac)
-      su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/power"
-      ;;
-  esac
-  logger "ACPI Event: $@"
+     ac-adapter)
+       su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/power"
+       ;;
+   esac
 }
+
 
 case "$@" in
   button/power*)                  ACPI_Event power button ;;
   button/volumeup*)               ACPI_Event volume output up ;;
   button/volumedown*)             ACPI_Event volume output down ;;
   button/mute*)                   ACPI_Event volume output mute ;;
-  video/tabletmode*TBLT)          ACPI_Event tablet mode ;;
-  ibm/hotkey*LEN0068:00*00006030) ACPI_EVENT ac adapter ;;
-  ibm/hotkey*LEN0068:00*0000500c) ACPI_EVENT stylus eject ;;
-  ibm/hotkey*LEN0068:00*00006030) ACPI_EVENT stylus replace ;;
-  *)                              ACPI_Event "$@" ;;
+  video/tabletmode*TBLT*)         ACPI_Event tablet ;;
+  ibm/hotkey*LEN0068:00*00006030) ACPI_EVENT ac-adapter ;;
+#  ibm/hotkey*LEN0068:00*0000500c) ACPI_EVENT stylus eject ;;
+#  ibm/hotkey*LEN0068:00*0000500b) ACPI_EVENT stylus replace ;;
 esac
-
+ 
+ 
 _Poweroff() {
   /sbin/shutdown -h -P now "Power button pressed"
 }
+
+
+
+# vim:set ts=4 sw=4 ft=sh et:
