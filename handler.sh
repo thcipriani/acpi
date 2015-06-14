a0cb7bd7e6da4039c3528263795b4f8b5074bc99
root <root@taskmaster.tylercipriani.com>
2015-06-14 15:03:24 -0700

    Fix up events

--- handler.sh
+++ handler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-user="$(w -h -s | awk '/:[0-9]/ {print $1}' | head -1)"
+user="$(w -h -s | awk '/:[0-9]/ {print $1; exit;}')"
 userhome=$(getent passwd $user | cut -d: -f6)
 
 logger "ACPI Event: $@"
@@ -12,32 +12,45 @@ ACPI_Event() {
         button) _Poweroff ;;
         ac) su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/power" ;;
       esac
-      ;;
+    ;;
 
-     volume)
-       case $2 in
-         output)
-           case $3 in
-             up)   su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/volume up"   ;;
-           down)   su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/volume down" ;;
-             mute) su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/volume mute" ;;
-           esac
-           ;;
-       esac
-       ;;
+    volume)
+      case $2 in
+        output)
+          case $3 in
+            up)   su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/volume up"   ;;
+          down)   su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/volume down" ;;
+            mute) su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/volume mute" ;;
+          esac
+          ;;
+      esac
+    ;;
 
     tablet) su $user -l -c "DISPLAY=:0.0 ${userhome}/bin/display tabletmode" ;;
+
+    brightness)
+        case $2 in
+            keyboard)
+                case $3 in
+                    up) DISPLAY=:0.0 $userhome/bin/keyboard-bl up ;;
+                    down) DISPLAY=:0.0 $userhome/bin/keyboard-bl down ;;
+                    revolve) DISPLAY=:0.0 $userhome/bin/keyboard-bl revolve ;;
+                esac
+            ;;
+        esac
+    ;;
    esac
 }
 
 
 case "$@" in
-  button/power*)                  ACPI_Event power button ;;
-  button/volumeup*)               ACPI_Event volume output up ;;
-  button/volumedown*)             ACPI_Event volume output down ;;
-  button/mute*)                   ACPI_Event volume output mute ;;
-  video/tabletmode*TBLT*)         ACPI_Event tablet ;;
-  ibm/hotkey\ LEN0068:00*6030)    ACPI_Event power ac ;;
+  button/power*)                   ACPI_Event power button ;;
+  button/volumeup*)                ACPI_Event volume output up ;;
+  button/volumedown*)              ACPI_Event volume output down ;;
+  button/mute*)                    ACPI_Event volume output mute ;;
+  video/tabletmode*TBLT*)          ACPI_Event tablet ;;
+  ibm/hotkey\ LEN0068:00*6030)     ACPI_Event power ac ;;
+  PNP0C14\:00\ 000000ff\ 00000000) ACPI_Event brightness keyboard revolve;;
 #  ibm/hotkey\ LEN0068:00*500c) ACPI_EVENT stylus eject ;;
 #  ibm/hotkey\ LEN0068:00*500b) ACPI_EVENT stylus replace ;;
 esac
